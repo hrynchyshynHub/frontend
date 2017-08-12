0a6d8bce1e876964a67a6d7f9df17de6bc08e280
hrynchyshynHub <vaniahrynchyshyn@gmail.com>
2017-08-12 14:17:40 +0300

    added list wirws

---
+++ src/app/last-reviews/last-reviews.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-last-reviews',
+  templateUrl: './last-reviews.component.html',
+  styleUrls: ['./last-reviews.component.css']
+})
+export class LastReviewsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
