39ae8abb66a469614909c654b2e2f3380e387d4c
hrynchyshynHub <vaniahrynchyshyn@gmail.com>
2017-08-12 14:15:30 +0300

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { ContentComponent } from './content/content.component';
+import { LastReviewsComponent } from './last-reviews/last-reviews.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    ContentComponent,
+    LastReviewsComponent,
   ],
   imports: [
-    BrowserModule
+    NgbModule.forRoot(), BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
